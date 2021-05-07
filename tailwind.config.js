2bbb350c35cd81a1ddf173e3c9c45a1a3da37850
Juan Tobon <juanbal2808@gmail.com>
2021-05-07 02:11:49 -0500

    Primer commit y configuracion inicial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx,vue}',],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
